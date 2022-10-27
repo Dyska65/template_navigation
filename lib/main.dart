285abd5b5373a7f88dc0a7a10e4bdda53516ee4b
@SvitlanaShh <dyska1809@gmail.com>
2022-10-27 11:47:59 +0300

    added restoratioin example

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:template_navigation/main_page.dart';
 import 'package:template_navigation/second_page.dart';
 import 'package:template_navigation/template.dart';
 import 'package:template_navigation/third_page.dart';
+import 'package:template_navigation/utils.dart';
 
 // After Hot Restart/Reload appear arrow back this fix: "leading: const SizedBox.shrink()". This happens only if use Navigater.push....
 // Controller not dispose after close screen if used GetMaterialApp with Navagator or Navigator GetX
@@ -33,6 +34,9 @@ class MyApp extends StatelessWidget {
         );
       }),
       onGenerateRoute: (settings) {
+        // print(settings.name);
+        // print(settings.arguments);
+        // },
         switch (settings.name) {
           case '/initial':
             return GetPageRoute(
@@ -87,31 +91,118 @@ class MyApp extends StatelessWidget {
           default:
           // MaterialPageRoute(builder: (_) => const ErrorPage());
         }
-        // return MaterialPageRoute(builder: (_) => const ErrorPage());
+        return MaterialPageRoute(builder: (_) => const ErrorPage());
       },
+      getPages: Routes.listOfRoutes,
     );
   }
 }
 
-  // getPages: Routes.listOfRoutes,
-  //   // unknownRoute: GetPage(name: '/error', page: () => const ErrorPage()),
-// routes: {
-//   '/initial': (context) => MainPage(),
-//   '/first_page': (context) => Template(body: FirstPage()),
-//   '/second_page': (context) => Template(body: SecondPage()),
-//   '/third_page': (context) => Template(body: ThirdPage()),
-// },
-// onGenerateRoute: (settings) {
-//   switch (settings.name) {
-//     case '/initial':
-//       MaterialPageRoute(builder: (_) => const FirstModulePage());
-//       break;
-//     case '/second_module/secon':
-//       MaterialPageRoute(builder: (_) => const SecondModule());
-//       break;
-//     default:
-//       MaterialPageRoute(builder: (_) => const ErrorPage());
-//   }
-//   return MaterialPageRoute(builder: (_) => const ErrorPage());
-// },
 
+
+  /* Setup for Restoration and how tested
+iOS:
+XCode in Runner-> Main ->Flutter View Controller-> restoration ID: "input your ID"
+Run only XCode after change state app, move to background this app,
+after stop app in XCode and open from background app
+ANDROID:
+Can open in debug mode, but emulator needed setup. In emulator open Settings-> Emulator-> Switch to Mode Developer->
+ ->Open Settings-> System-> For Developer -> Switch to "Don't keep activities"
+ Run F5 changed state and move to background this app, after open 
+ WEB:
+Run only from terminal with command flutter run --profile or flutter run --release, 
+after go to another page and back, after changed state and tap to back or forward button 
+and after back to current page and you can see old state
+ */
+
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const RestorationExampleApp());
+
+// class RestorationExampleApp extends StatelessWidget {
+//   const RestorationExampleApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       restorationScopeId: 'app',
+//       title: 'Restorable Counter',
+//       home: RestorableCounter(restorationId: 'counter'),
+//     );
+//   }
+// }
+
+// class RestorableCounter extends StatefulWidget {
+//   const RestorableCounter({super.key, this.restorationId});
+
+//   final String? restorationId;
+
+//   @override
+//   State<RestorableCounter> createState() => _RestorableCounterState();
+// }
+
+// // The [State] object uses the [RestorationMixin] to make the current value
+// // of the counter restorable.
+// class _RestorableCounterState extends State<RestorableCounter>
+//     with RestorationMixin {
+//   // The current value of the counter is stored in a [RestorableProperty].
+//   // During state restoration it is automatically restored to its old value.
+//   // If no restoration data is available to restore the counter from, it is
+//   // initialized to the specified default value of zero.
+//   final RestorableInt _counter = RestorableInt(0);
+
+//   // In this example, the restoration ID for the mixin is passed in through
+//   // the [StatefulWidget]'s constructor.
+//   @override
+//   String? get restorationId => widget.restorationId;
+
+//   @override
+//   void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+//     // All restorable properties must be registered with the mixin. After
+//     // registration, the counter either has its old value restored or is
+//     // initialized to its default value.
+//     registerForRestoration(_counter, 'count');
+//   }
+
+//   void _incrementCounter() {
+//     setState(() {
+//       // The current value of the property can be accessed and modified via
+//       // the value getter and setter.
+//       _counter.value++;
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     _counter.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Restorable Counter'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '${_counter.value}',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
